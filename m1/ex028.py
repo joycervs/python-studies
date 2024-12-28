18c76d75460288052c8e6861927eda1e4e61d211
joyceravie <joyce.rvs@hotmail.com>
2024-12-27 21:32:40 -0300

    Finishing Mundo 1 - Python

--- m1/ex028.py
+++ m1/ex028.py
@@ -3,15 +3,15 @@ from time import sleep
 
 n = randint(0, 5)
 
-print('-=-' * 20)
-print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
-print('-=-' * 20)
+print('\033[0;36m-=-' * 20)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...\033[m')
+print('\033[0;36m-=-\033[m' * 20)
 
-num = int(input('Em que número eu pensei? '))
-print('PROCESSANDO...')
+num = int(input('\033[0;35mEm que número eu pensei? '))
+print('PROCESSANDO...\033[m')
 sleep(2)
 
 if num == n: 
-  print('PARABÉNS, você conseguiu me vencer!')
+  print('\033[1;32mPARABÉNS, você conseguiu me vencer!\033[m')
 else:
-  print(f'GANHEI! Eu pensei no número {n} e não no {num}!')
+  print(f'\033[1;31mGANHEI! Eu pensei no número {n} e não no {num}!\033[m')
--- m1/ex029.py
+++ m1/ex029.py
@@ -2,6 +2,6 @@ velocidade = float(input('Qual é a velocidade atual do carro? '))
 
 if velocidade > 80:
   multa = (velocidade - 80) * 7.00
-  print('MULTADO! Você excedeu o limite permitido que é de 80Km/h')
-  print(f'Você vai pagar uma multa de R${multa:.2f}')
+  print('\033[1;31mMULTADO! Você excedeu o limite permitido que é de 80Km/h')
+  print(f'Você vai pagar uma multa de R${multa:.2f}\033[m')
 print('Tenha um bom dia! Dirija com segurança!')
